56f66c460c57d87b13d8cb66dc1b3b9a3aa34ab7
Satyam Singhal <33842692+Stysingh98@users.noreply.github.com>
2023-02-15 22:20:55 +0530

    change 2

--- editthis.js
+++ editthis.js
@@ -5,20 +5,20 @@ var site = {
        /* This decription is used both in meta tag and about page */
     description: `Hi there! I am Satyam Singhal from Meerut, India.
     I have done Bachelor of Technology in Electronics and communication from IIIT Allahabad.
-    I ❤️ to design Websites, Programming stuff, and Photography.`,
+    I ❤️ to do MAchine learning Stuff, Programming stuff, and Photography.`,
     /* Change your skills and title accodingly */
-    skills_title: ['Web Development', 'Design','Programming'],
-    skills: [['HTML', 'CSS', 'JS', 'ReactJS','SCSS'], ['Photoshop', 'XD','Figma'],['C','C++','Python3', 'Javascript']],
+    skills_title: ['Interest', 'Tools','Programming'],
+    skills: [['Machine Learning','Deep Learning'], ['Silvaco', 'Xilink Vivado', 'LTspice', 'Aim spice', 'MATLAB', 'Wireshark'],['C','C++','VeriLog','MYSQL']],
     skillp:[[100, 80 ,100, 60, 80], [50, 60, 50], [70,80,90,90]],
     social: {
         /* Add your social media links here */
-        github: 'https://github.com/adithyapaib',
-        twitter: 'https://twitter.com/adithyapaib',
-        linkedin: 'https://www.linkedin.com/in/adithyapaib/',
-        mail: 'mailto: email@gmail.com',
-        instagram: 'https://instagram.com'
+        github: 'https://github.com/Stysingh98',
+        twitter: 'https://twitter.com/Styallen1998?t=ddAURJgQTRs29ALflM749w&s=08',
+        linkedin: 'https://www.linkedin.com/in/satyam-singhal-43b74a1b0/',
+        mail: 'mailto: stysingh.98@gmail.com',
+        instagram: 'https://instagram.com/stysingh.98?igshid=ZDdkNTZiNTM='
     },
-    form: 'https://formsubmit.co/email@gmail.com',
+    form: 'https://formsubmit.co/stysingh.98@gmail.com',
     /* DONT FOR GET TO ACTIVATE UR EMAIL AFTER SETTING UP THE FORM TO RECEIVE EMAILS */
     projects: [
         ["https://google.com", 'https://github.com/adithyapaib/portfolio-template',"https://github.com/adithyapaib/portfolio-template", "https://github.com/adithyapaib/portfolio-template"],
